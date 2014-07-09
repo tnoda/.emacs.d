69c6fa70fbfc19b2a412c44862e1fdf527ddaf76
Takahiro Noda <takahiro.noda+github@gmail.com>
2014-07-09 15:30:42 +0900

    Stop requiring cl in init.el.

--- init.el
+++ init.el
@@ -3,7 +3,6 @@
 ;; This file loads Org-mode and then loads the rest of our Emacs initialization from Emacs lisp
 ;; embedded in literate Org-mode files.
 
-(require 'cl)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; 

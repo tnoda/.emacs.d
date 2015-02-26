eb4b75d8afe7874c561de47e53007e71ffa3146d
Takahiro Noda <takahiro.noda+github@gmail.com>
2015-02-26 18:55:22 +0900

    List the Org mode archive

--- init.el
+++ init.el
@@ -55,6 +55,7 @@
   (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
   (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
   (package-initialize))
 
 (if (require 'cask tnoda/cask-el-path t)
